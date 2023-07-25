0d2d379e2e3d0d4ab940eb1c4004c8b66b0d3cd6
NurMustafaM <nor.mohamad@finanzero.com.br>
2023-07-25 13:33:21 -0300

    resolve a mistake and modified to true or false

--- swagger.js
+++ swagger.js
@@ -1,16 +1,21 @@
 const swaggerAutogen = require("swagger-autogen")();
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const fs = require('fs');
-const endpointsFiles = ["./routes/routes.js"];
+const app = require('./app');
+
+const application = express();
+const port = 3000;
 
 const swaggerOutputDir = "./swagger";
-const swaggerOutputFileName = "swagger_output.json";
+const swaggerOutputFileName = `swagger_output_${getDateFormatted()}.json`;
 const swaggerOutputFile = `${swaggerOutputDir}/${swaggerOutputFileName}`;
 
+let swaggerDocument = {};
 
 const autonomousUpload = false; // Defina como 'true' para habilitar ou 'false' para desabilitar
 
-function generateSwagger(endpointsFiles, autonomousUpload) {
-
+function generateSwagger() {
   if (!autonomousUpload) {
     console.log("Autonomous upload is disabled. Skipping Swagger JSON generation.");
     return;
@@ -37,21 +42,33 @@ function generateSwagger(endpointsFiles, autonomousUpload) {
     paths: {},
   };
 
-  swaggerAutogen(swaggerOutputFile, endpointsFiles, doc)
+  swaggerAutogen(swaggerOutputFile, [endpointsFiles], doc)
     .then(() => {
       console.log("Swagger JSON file has been generated");
+      // Start the server and serve the Swagger UI after generating the Swagger JSON
+      startServer();
     })
     .catch((error) => {
       console.error("Error generating Swagger JSON file:", error);
     });
 }
 
-generateSwagger(endpointsFiles, autonomousUpload); // Generate the Swagger JSON
+function startServer() {
+  // Check if the Swagger JSON file exists
+  if (fs.existsSync(swaggerOutputFile)) {
+    swaggerDocument = require(`./${swaggerOutputFile}`);
+  } else {
+    console.log(`Swagger file '${swaggerOutputFile}' does not exist. Generating new file...`);
+    generateSwagger();
+  }
+}
+
+generateSwagger(); // Start the process by generating the Swagger JSON
 
 function getDateFormatted() {
   const date = new Date();
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const day = date.getDate().toString().padStart(2, '0');
-  return `${day}${month}${year}`;
+  return `${day}_${month}_${year}`;
 }
