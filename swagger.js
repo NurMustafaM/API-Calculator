8914c722ff9d824df9bb315484fd7ca6002f00c8
NurMustafaM <nor.mohamad@finanzero.com.br>
2023-07-25 14:51:25 -0300

    loop infinity solved

--- swagger.js
+++ swagger.js
@@ -1,23 +1,16 @@
 const swaggerAutogen = require("swagger-autogen")();
-const express = require('express');
-const swaggerUi = require('swagger-ui-express');
 const fs = require('fs');
-const app = require('./app');
-
-const application = express();
-const port = 3000;
+const endpointsFiles = ["./routes/routes.js"];
 
 const swaggerOutputDir = "./swagger";
-const swaggerOutputFileName = `swagger_output_${getDateFormatted()}.json`;
+const swaggerOutputFileName = "swagger_output.json"; 
 const swaggerOutputFile = `${swaggerOutputDir}/${swaggerOutputFileName}`;
 
-let swaggerDocument = {};
 
 const autonomousUpload = true; // Defina como 'true' para habilitar ou 'false' para desabilitar
 
-const endpointsFiles = ["./routes/routes.js"];
-
-function generateSwagger() {
+function generateSwagger(endpointsFiles, autonomousUpload) {
+ 
   if (!autonomousUpload) {
     console.log("Autonomous upload is disabled. Skipping Swagger JSON generation.");
     return;
@@ -44,39 +37,21 @@ function generateSwagger() {
     paths: {},
   };
 
-  swaggerAutogen(swaggerOutputFile, [endpointsFiles], doc)
+  swaggerAutogen(swaggerOutputFile, endpointsFiles, doc)
     .then(() => {
       console.log("Swagger JSON file has been generated");
-    
-      startServer();
     })
     .catch((error) => {
       console.error("Error generating Swagger JSON file:", error);
     });
 }
 
-function startServer() {
-  // Check if the Swagger JSON file exists
-  if (fs.existsSync(swaggerOutputFile)) {
-    swaggerDocument = require(`./${swaggerOutputFile}`);
-  } else {
-    console.log(`Swagger file '${swaggerOutputFile}' does not exist. Generating new file...`);
-    generateSwagger();
-  }
-}
-
-generateSwagger(); 
-
-application.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-const server = application.listen(port, () => {
-  console.log(`Swagger UI is available at http://localhost:${server.address().port}/api-docs`);
-});
+generateSwagger(endpointsFiles, autonomousUpload); 
 
 function getDateFormatted() {
   const date = new Date();
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const day = date.getDate().toString().padStart(2, '0');
-  return `${day}_${month}_${year}`;
+  return `${day}${month}${year}`;
 }
